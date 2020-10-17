dcba2040ecc538e0075fc91712ce20c11ec96d4b
zhengjianliu <comezheng@gmail.com>
2020-10-17 15:00:53 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,24 @@
 const returnFirstTwoDrivers = (arr) =>{
   return arr.slice(0,2)
 }
+
+const returnLastTwoDrivers = (arr) =>{
+  return arr.slice(-2)
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = (value1) =>{
+  return function(value2){
+    return value1 * value2
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers =  (array, driversFunction) =>{
+  return driversFunction(array)
+}
